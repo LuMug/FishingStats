1eca7ec31bdd636cf4d5a99ad93ed21133c25b3a
dafo90 <41897414+dafo90@users.noreply.github.com>
2020-04-01 18:21:50 +0200

    First backend commit

---
+++ backend/src/logger.js
@@ -0,0 +1,18 @@
+const log4js = require('log4js');
+
+log4js.configure({
+    appenders: {
+        out: {
+            type: 'console',
+            layout: {
+                type: 'pattern',
+                pattern: '%d{ISO8601}%d{O} - %-5p - [%c] - %m'
+            }
+        }
+    },
+    categories: {
+        default: { appenders: ['out'], level: 'info' }
+    }
+});
+
+module.exports = log4js;
